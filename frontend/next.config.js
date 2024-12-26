fb875ca38bd5c2a8b5d9e5e66ef5d184cb7935a4
Virtual Alpha <146192632+virtual-alphas@users.noreply.github.com>
2024-12-26 14:40:10 -0600

    update frontend configuration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  target: 'serverless',
-  output: 'export',
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
